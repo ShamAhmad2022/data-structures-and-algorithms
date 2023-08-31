93c785acadb1aa26e7492cea4f054daf6cd1f2c8
ShamAhmad2022 <shamahmadaljalam@gmail.com>
2023-08-31 20:59:47 +0000

    hashtables

--- javascript/code-challenges/SortingComparisons/index.js
+++ javascript/code-challenges/SortingComparisons/index.js
@@ -1,52 +1,36 @@
-'use strict';
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-const movies = [
-  {
-    title: 'Jurassic Park',
-    year: 1993,
-    genres: ['Action', 'Adventure', 'Sci-Fi'],
-  },
-  {
-    title: 'Inception',
-    year: 2010,
-    genres: ['Action', 'Sci-Fi', 'Adventure'],
-  },
-  {
-    title: 'The Dark Knight',
-    year: 2008,
-    genres: ['Action', 'Crime', 'Drama'],
-  },
-  {
-    title: 'The Shawshank Redemption',
-    year: 1994,
-    genres: ['Drama'],
-  },
-  {
-    title: 'Pulp Fiction',
-    year: 1994,
-    genres: ['Crime', 'Drama'],
-  },
-];
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
 
-function sortByYear(array){
-  return array.sort((a, b) => b.year - a.year);
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
 }
 
-console.log(sortByYear(movies));
+function merge(left, right) {
+  let i = 0;
+  let j = 0;
+  let mergedArr = [];
 
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      mergedArr.push(left[i]);
+      i++;
+    } else {
+      mergedArr.push(right[j]);
+      j++;
+    }
+  }
 
-function removeLeadingArticles(title) {
-  return title.replace(/^(A|An|The)\s+/i, '');
+  return mergedArr.concat(left.slice(i)).concat(right.slice(j));
 }
 
-
-function sortByTitle(array){
-  return array.sort((a, b) => {
-    const titleA = removeLeadingArticles(a.title);
-    const titleB = removeLeadingArticles(b.title);
-    return titleA.localeCompare(titleB);
-  });
-}
-
-console.log('..................................');
-console.log(sortByTitle(movies));
+const unsortedArray = [8, 4, 23, 42, 16, 15];
+const sortedArray = mergeSort(unsortedArray);
+console.log(sortedArray);
